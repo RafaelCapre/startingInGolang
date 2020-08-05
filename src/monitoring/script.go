368ab21fe1965268f6fc951e2f956dc9e497940e
Rafael Cardoso Pereira <rafael.pereira@easynvest.com.br>
2020-08-05 00:09:33 -0300

    logs

--- src/monitoring/script.go
+++ src/monitoring/script.go
@@ -4,13 +4,15 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
-const monitoramentos int = 2
+const monitoramentos = 2
 const delay = 5
 
 func main() {
@@ -18,6 +20,7 @@ func main() {
 
 	for {
 		exibeMenu()
+
 		comando := leComando()
 
 		switch comando {
@@ -25,6 +28,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exibindo Logs...")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo do programa")
 			os.Exit(0)
@@ -37,8 +41,8 @@ func main() {
 }
 
 func exibeIntroducao() {
-	nome := "Rafael"
-	versao := 1.1
+	nome := "Douglas"
+	versao := 1.2
 	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
 }
@@ -53,31 +57,13 @@ func leComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido)
 	fmt.Println("O comando escolhido foi", comandoLido)
+	fmt.Println("")
 
 	return comandoLido
 }
 
-func testaSite(site string) {
-
-	//Capturando o segundo parâmetro
-	resp, err := http.Get(site)
-	//Verificando se houve algum erro
-	if err != nil {
-		fmt.Println("Ocorreu um erro:", err)
-	}
-
-	if resp.StatusCode == 200 {
-		fmt.Println("Site:", site, "foi carregado com sucesso!")
-		registralog(site, true)
-	} else {
-		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
-		registralog(site, false)
-	}
-}
-
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-
 	sites := leSitesDoArquivo()
 
 	for i := 0; i < monitoramentos; i++ {
@@ -88,13 +74,28 @@ func iniciarMonitoramento() {
 		time.Sleep(delay * time.Second)
 		fmt.Println("")
 	}
+
 	fmt.Println("")
 }
 
+func testaSite(site string) {
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+		registraLog(site, true)
+	} else {
+		fmt.Println("Site:", site, "esta com problemas. Status Code:", resp.StatusCode)
+		registraLog(site, false)
+	}
+}
+
 func leSitesDoArquivo() []string {
-
 	var sites []string
-
 	arquivo, err := os.Open("sites.txt")
 
 	if err != nil {
@@ -118,13 +119,27 @@ func leSitesDoArquivo() []string {
 	return sites
 }
 
-func registralog(site string, status bool) {
+func registraLog(site string, status bool) {
 
-	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE, 0666)
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(arquivo)
+	arquivo.WriteString(time.Now().Format("04/08/2020 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
+
+	arquivo.Close()
+}
+
+func imprimeLogs() {
+
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(arquivo))
+
 }
